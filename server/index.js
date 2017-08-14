8fb11e7305aed942bb0f15853a451be28b0c5708
Brendan Myers <me@brendamn.com>
2017-08-15 09:46:11 +1000

    Remove tick generator (too slow). Fix reg addresses.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.static('../dist'));
+app.use(express.static('../src'));
 
 app.get('/', (req, res) =>
   res.redirect('/dist')
